add4673f97415cf854c7bb275ff34ed66f91178c
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-05-22 15:56:19 -0500

    test

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,6 @@ var $container = $('.container');
 var $searchBtn = $('.searchBtn');
 var $deathSelect = $('.deathSelect');
 var $body = $('body');
-var glob;
 
 $( document ).ready(function() {
     $searchBtn.click((event)=>{
@@ -13,11 +12,22 @@ $( document ).ready(function() {
         $.get(`https://hidden-plateau-56299.herokuapp.com/api/dead_celeb/`, (data) => {
     //  var results = JSON.parse(data) 
                 var results = data;
-                glob = results;
                 console.log('These are the results: ', results);
 
                 createProfile();
-                console.log('Death Select Value ', deathSelect.val());
+                console.log('Death Select Value toLowerCase ', $deathSelect.val().toLowerCase);
+
+                switch($deathSelect.val().toLowerCase()) {
+                    case 'drug_overdose': console.log('drug overdose');
+                    case 'suicide': console.log('suicide');
+                    case 'murder': console.log('murder');
+                    case 'plane_crash': console.log('plane_crash');
+                    case 'skiing_accident': console.log('skiing_accident');
+                    case 'botched_surgery': console.log('botched surgery');
+                    case 'natural_causes': console.log('natural causes');
+                    case 'aids': console.log('aids');
+                    case 'car_crash': console.log('car crash');
+                }
             });
     });
 });

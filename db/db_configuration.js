c789f76e6c1c49eab967c00ef64614c7970a29ee
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-05-22 12:53:13 -0500

    make it work

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//     database: 'dead_celeb',
-//     port:5432
-// });
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    database: 'dead_celeb',
+    port:5432
 });
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
 
 // const pool = new Pool({
 //     user: process.env.DATABASE_USER, 

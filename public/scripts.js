9abdceff313f398e8a496437eafdab7f54068b0d
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-05-22 20:25:09 -0500

    test

--- public/scripts.js
+++ public/scripts.js
@@ -8,46 +8,45 @@ var deathNumber = 0;
 $( document ).ready(function() {
     $searchBtn.click((event)=>{
     let deathSelectVal = $deathSelect.val().toLowerCase();
-    
 
+          switch(deathSelectVal) {
+               case 0: deathSelectVal === 'drug_overdose';
+                    deathNumber = 111;
+                    console.log('drug overdose');
+               case 1: deathSelectVal === 'suicide';
+                    deathNumber = 222;
+                    console.log('suicide');
+               case 2: deathSelectVal === 'murder';
+                    deathNumber = 333;
+                    console.log('murder');
+               case 3: deathSelectVal === 'plane_crash';
+                    deathNumber = 444;
+                    console.log('plane_crash');
+               case 4: deathSelectVal === 'skiing_accident';
+                    deathNumber = 555;
+                    console.log('skiing_accident');
+               case 5: deathSelectVal === 'botched_surgery';
+                    deathNumber = 666;
+                    console.log('botched surgery');
+               case 6: deathSelectVal === 'natural_causes';
+                    deathNumber = 777;
+                    console.log('natural causes');
+               case 7: deathSelectVal === 'aids';
+                    deathNumber = 888;
+                    console.log('aids');
+               case 8: deathSelectVal === 'car_crash';
+                    deathNumber = 999;
+                    console.log('car crash');
+          }
+
+          console.log(deathNumber);
+    
         //http://cors-anywhere.herokuapp.com/
 
         $.get(`https://hidden-plateau-56299.herokuapp.com/api/dead_celeb/`, (data) => {
     //  var results = JSON.parse(data)
                 results = data;
                 console.log('These are the results: ', results);
-
-                console.log($deathSelect.val());
-
-                switch(deathSelectVal) {
-                    case 0: deathSelectVal === 'drug_overdose';
-                         deathNumber = 111;
-                         console.log('drug overdose');
-                    case 1: deathSelectVal === 'suicide';
-                         deathNumber = 222;
-                         console.log('suicide');
-                    case 2: deathSelectVal === 'murder';
-                         deathNumber = 333;
-                         console.log('murder');
-                    case 3: deathSelectVal === 'plane_crash';
-                         deathNumber = 444;
-                         console.log('plane_crash');
-                    case 4: deathSelectVal === 'skiing_accident';
-                         deathNumber = 555;
-                         console.log('skiing_accident');
-                    case 5: deathSelectVal === 'botched_surgery';
-                         deathNumber = 666;
-                         console.log('botched surgery');
-                    case 6: deathSelectVal === 'natural_causes';
-                         deathNumber = 777;
-                         console.log('natural causes');
-                    case 7: deathSelectVal === 'aids';
-                         deathNumber = 888;
-                         console.log('aids');
-                    case 8: deathSelectVal === 'car_crash';
-                         deathNumber = 999;
-                         console.log('car crash');
-                }
 
                 createProfile();  
                });

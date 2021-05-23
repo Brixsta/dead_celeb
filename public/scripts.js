377961736a28e88d0bfdc68ee7a2a3661cb5afe0
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-05-22 20:08:30 -0500

    test

--- public/scripts.js
+++ public/scripts.js
@@ -22,7 +22,39 @@ $( document ).ready(function() {
                 
 
                     function createProfile() {
-                         console.log('DEATH NUMBER IS: ', deathNumber);
+                         
+                         switch(deathSelectVal) {
+                              case 0: $deathSelect.val().toLowerCase() === 'drug_overdose';
+                                   deathNumber = 111;
+                                   console.log('drug overdose');
+                              case 1: $deathSelect.val().toLowerCase() === 'suicide';
+                                   deathNumber = 222;
+                                   console.log('suicide');
+                              case 2: $deathSelect.val().toLowerCase() === 'murder';
+                                   deathNumber = 333;
+                                   console.log('murder');
+                              case 3: $deathSelect.val().toLowerCase() === 'plane_crash';
+                                   deathNumber = 444;
+                                   console.log('plane_crash');
+                              case 4: $deathSelect.val().toLowerCase() === 'skiing_accident';
+                                   deathNumber = 555;
+                                   console.log('skiing_accident');
+                              case 5: $deathSelect.val().toLowerCase() === 'botched_surgery';
+                                   deathNumber = 666;
+                                   console.log('botched surgery');
+                              case 6: $deathSelect.val().toLowerCase() === 'natural_causes';
+                                   deathNumber = 777;
+                                   console.log('natural causes');
+                              case 7: $deathSelect.val().toLowerCase() === 'aids';
+                                   deathNumber = 888;
+                                   console.log('aids');
+                              case 8: $deathSelect.val().toLowerCase() === 'car_crash';
+                                   deathNumber = 999;
+                                   console.log('car crash');
+                          }
+                         
+                          console.log('DEATH NUMBER IS: ', deathNumber);
+
                          var $celebBox = $('<div></div>', {class:'celebBox'});
                          var $celebHeading = $('<h1></h1>', {class:'celebHeading', text:`Celebrity deaths: ${$deathSelect.val().toLowerCase().replace('_', ' ')}`});
                          var $celebDivider = $('<hr>', {class:'celebDivider'});
@@ -38,36 +70,6 @@ $( document ).ready(function() {
                     
                     function listOfDead (array, deathIdNum) {
                               let newArr = [];
-
-                              switch(deathSelectVal) {
-                                   case 0: $deathSelect.val().toLowerCase() === 'drug_overdose';
-                                        deathNumber = 111;
-                                        console.log('drug overdose');
-                                   case 1: $deathSelect.val().toLowerCase() === 'suicide';
-                                        deathNumber = 222;
-                                        console.log('suicide');
-                                   case 2: $deathSelect.val().toLowerCase() === 'murder';
-                                        deathNumber = 333;
-                                        console.log('murder');
-                                   case 3: $deathSelect.val().toLowerCase() === 'plane_crash';
-                                        deathNumber = 444;
-                                        console.log('plane_crash');
-                                   case 4: $deathSelect.val().toLowerCase() === 'skiing_accident';
-                                        deathNumber = 555;
-                                        console.log('skiing_accident');
-                                   case 5: $deathSelect.val().toLowerCase() === 'botched_surgery';
-                                        deathNumber = 666;
-                                        console.log('botched surgery');
-                                   case 6: $deathSelect.val().toLowerCase() === 'natural_causes';
-                                        deathNumber = 777;
-                                        console.log('natural causes');
-                                   case 7: $deathSelect.val().toLowerCase() === 'aids';
-                                        deathNumber = 888;
-                                        console.log('aids');
-                                   case 8: $deathSelect.val().toLowerCase() === 'car_crash';
-                                        deathNumber = 999;
-                                        console.log('car crash');
-                               }
 
                               for(let i=0; i<array.length; i++) {
                               if(array[i].deathid === deathIdNum) {

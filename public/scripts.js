68948321ca4820d69d393f61048a4f8d46cb211c
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-05-23 21:35:44 -0500

    test

--- public/scripts.js
+++ public/scripts.js
@@ -154,22 +154,14 @@ function rowAmtToCreate () {
 
 function bloodSplatterEffects () {
      
-
-
-     console.log('bloodsplatter executed');
-
-     
-
-
      $('.bloodSplatter0').hide();
      $('.bloodSplatter1').hide();
      $('.bloodSplatter2').hide();
      $('.bloodSplatter3').hide();
      
-     
      fadeInAndOut();
      setTimeout(function(){
-          bloodSplatterEffects();
+     bloodSplatterEffects();
      },24500);
 }
 
